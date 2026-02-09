31b37e288bc78a3b8b9c0c6f2beac7350a7c47b9
Olivia Neacsu <olivia.neacsu@gmail.com>
2026-02-09 11:31:29 +0100

    Övning vecka 3

--- IdentityDemo/Program.cs
+++ IdentityDemo/Program.cs
@@ -1,32 +1,39 @@
 using IdentityDemo.Data;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 using System.Threading.Tasks;
 
 namespace IdentityDemo
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            // Add services to the container. 
+            var connectionString =
+            builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+            options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            builder.Services.AddRazorPages();
-
+            // Add Identity services 
+            builder.Services
+            .AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+            .AddMicrosoftIdentityWebApp(options =>
+            {
+                builder.Configuration.Bind("AzureAd", options);
+                options.TokenValidationParameters.RoleClaimType =
+    "http://schemas.microsoft.com/ws/2008/06/identity/claims/role";
+            });
+            builder.Services.AddAuthorization();
+            builder.Services.AddRazorPages()
+            .AddMicrosoftIdentityUI();
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
-
-            await IdentitySeed.SeedAsync(app.Services);
-
-            // Configure the HTTP request pipeline.
+            // Configure the HTTP request pipeline. 
             if (app.Environment.IsDevelopment())
             {
                 app.UseMigrationsEndPoint();
@@ -34,21 +41,20 @@ namespace IdentityDemo
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see 
+            https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
-
             app.MapStaticAssets();
             app.MapRazorPages()
-               .WithStaticAssets();
-
+            .WithStaticAssets();
+            app.MapControllers();
             app.Run();
         }
+
     }
 }
